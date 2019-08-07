64c8a224c9c5e856827eff904bd743c4c1830eaa
ivanasen <ivanasenchakarov@gmail.com>
2019-08-07 12:35:36 +0300

    Add TerminalManager to repository

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:wetonomy/repositories/shared_preferences_terminal_manager.dart';
 import 'package:wetonomy/repositories/strongforce_api_client_mock.dart';
 import 'package:wetonomy/repositories/strongforce_repository.dart';
 import 'package:wetonomy/widgets/app.dart';
@@ -8,9 +10,12 @@ import 'package:wetonomy/widgets/app.dart';
 import 'bloc/contracts_bloc.dart';
 import 'logging_bloc_delegate.dart';
 
-void main() {
-  final StrongForceRepository repository =
-      StrongForceRepository(StrongForceApiClientMock());
+//TODO: Add a DI Container
+void main() async {
+  SharedPreferences sharedPrefs = await SharedPreferences.getInstance();
+  final StrongForceRepository repository = StrongForceRepository(
+      StrongForceApiClientMock(),
+      SharedPreferencesTerminalManager(sharedPrefs));
 
   BlocSupervisor.delegate = LoggingBlocDelegate();
 
--- lib/repositories/strongforce_repository.dart
+++ lib/repositories/strongforce_repository.dart
@@ -1,20 +1,37 @@
 import 'package:wetonomy/models/contract_action.dart';
 import 'package:wetonomy/models/query.dart';
 import 'package:wetonomy/models/result.dart';
+import 'package:wetonomy/models/terminal.dart';
 import 'package:wetonomy/repositories/strongforce_api_client.dart';
+import 'package:wetonomy/repositories/terminal_manager.dart';
 
 class StrongForceRepository {
-  final StrongForceApiClient client;
+  final StrongForceApiClient _client;
+  final TerminalManager _terminalManager;
 
-  StrongForceRepository(this.client) : assert(client != null);
+  StrongForceRepository(this._client, this._terminalManager)
+      : assert(_client != null),
+        assert(_terminalManager != null);
 
   Future<Result> sendAction(ContractAction action) async {
-    final Result result = await client.sendAction(action);
+    final Result result = await _client.sendAction(action);
     return result;
   }
 
   Future<Result> sendQuery(Query query) async {
-    final Result result = await client.sendQuery(query);
+    final Result result = await _client.sendQuery(query);
     return result;
+  }
+
+  Future<bool> addTerminal(Terminal terminal) {
+    return _terminalManager.addTerminal(terminal);
+  }
+
+  Future<bool> removeTerminal(Terminal terminal) {
+    return _terminalManager.removeTerminal(terminal);
+  }
+
+  Future<Set<Terminal>> getAllTerminals() {
+    return _terminalManager.getAllTerminals();
   }
 }
