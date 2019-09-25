bbc0d5647550b4a0b166994610a4a8339f0083d7
ivanasen <ivanasenchakarov@gmail.com>
2019-09-25 15:13:18 +0300

    Start using fake api when in debug mode

--- lib/services/service_locator.dart
+++ lib/services/service_locator.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wetonomy/repositories/terminals_repository.dart';
+import 'package:wetonomy/services/contracts_api_client.dart';
 import 'package:wetonomy/services/fake_contracts_api_client.dart';
 import 'package:wetonomy/services/shared_preferences_terminals_dao.dart';
 import 'package:wetonomy/services/strongforce_api_client_cosmos.dart';
@@ -15,7 +17,14 @@ Future<void> setupServiceLocator() async {
 Future<void> _registerTerminalsRepository() async {
   final SharedPreferences sharedPrefs = await SharedPreferences.getInstance();
   final terminalManager = SharedPreferencesTerminalsDao(sharedPrefs);
-  final apiClient = FakeContractsApiClient();
+
+  ContractsApiClient apiClient;
+  if (kDebugMode) {
+    apiClient = FakeContractsApiClient();
+  } else {
+    apiClient = StrongForceApiClientCosmos();
+  }
+
   locator.registerSingleton<TerminalsRepository>(
       TerminalsRepository(terminalManager, apiClient));
 }
