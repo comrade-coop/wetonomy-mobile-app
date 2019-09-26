517b509f04c5d82e1ff8502f96cb23cd1408258a
ivanasen <ivanasenchakarov@gmail.com>
2019-09-26 11:25:21 +0300

    Revert to CosmosApiClient

--- lib/services/service_locator.dart
+++ lib/services/service_locator.dart
@@ -1,9 +1,7 @@
-import 'package:flutter/foundation.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:wetonomy/repositories/terminals_repository.dart';
 import 'package:wetonomy/services/contracts_api_client.dart';
-import 'package:wetonomy/services/fake_contracts_api_client.dart';
 import 'package:wetonomy/services/shared_preferences_terminals_dao.dart';
 import 'package:wetonomy/services/strongforce_api_client_cosmos.dart';
 import 'package:wetonomy/repositories/repositories.dart';
@@ -18,12 +16,7 @@ Future<void> _registerTerminalsRepository() async {
   final SharedPreferences sharedPrefs = await SharedPreferences.getInstance();
   final terminalManager = SharedPreferencesTerminalsDao(sharedPrefs);
 
-  ContractsApiClient apiClient;
-  if (kDebugMode) {
-    apiClient = FakeContractsApiClient();
-  } else {
-    apiClient = StrongForceApiClientCosmos();
-  }
+  ContractsApiClient apiClient = StrongForceApiClientCosmos();
 
   locator.registerSingleton<TerminalsRepository>(
       TerminalsRepository(terminalManager, apiClient));
